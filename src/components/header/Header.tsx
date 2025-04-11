d29e8a8587c9ba6a7cda14030c6a510661eddc5f
Gulmira <uraimovagulmira081282@gmail.com>
2025-04-11 14:14:21 +0600

    change adaptation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,20 @@
+import AdbIcon from '@mui/icons-material/Adb'
+import MenuIcon from '@mui/icons-material/Menu'
+import SearchIcon from '@mui/icons-material/Search'
+import { IconButton, InputBase, Menu } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
+import MenuItem from '@mui/material/MenuItem'
+import { alpha, styled } from '@mui/material/styles'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import SearchIcon from '@mui/icons-material/Search'
-import { InputBase } from '@mui/material'
-import { alpha, styled } from '@mui/material/styles'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { searchProducts } from '../../features/slices/ProductSlice'
+import useDebounce, { useAppDispatch } from '../../hooks/redux-hooks'
 import { pages } from '../constants/Constants'
 import './Header.css'
-// import { useEffect, useState } from 'react'
-// import { useAppDispatch } from '../../hooks/redux-hooks'
-// import { searchProducts } from '../../features/slices/ProductSlice'
-// import useDebounce from '../../hooks/redux-hooks'
 
 const Search = styled('div')(({ theme }) => ({
 	position: 'relative',
@@ -54,26 +56,31 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 	},
 }))
 
-
-
 function Header() {
-	// const [query, setQuery] = useState('')
-	// const dispatch = useAppDispatch()
-    // const debounceQuery = useDebounce(query, 400)
+	const [query, setQuery] = useState('')
+	const dispatch = useAppDispatch()
+	const debounceQuery = useDebounce(query, 400)
 
+	const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
+	const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorElNav(event.currentTarget)
+	}
 
-	// useEffect(() => {
-	// 	if (debounceQuery) {
-	// 		dispatch(searchProducts(debounceQuery))
-	// 	}
-		
-	// }, [debounceQuery, dispatch])
-	
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null)
+	}
+
+	useEffect(() => {
+		if (debounceQuery) {
+			dispatch(searchProducts(debounceQuery))
+		}
+	}, [debounceQuery, dispatch])
 
 	return (
 		<AppBar position='static'>
 			<Container maxWidth='xl' className='header'>
 				<Toolbar disableGutters>
+					<AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
 					<Typography
 						variant='h5'
 						noWrap
@@ -92,12 +99,69 @@ function Header() {
 						UNISTORE
 					</Typography>
 
-				
-					
-					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }}}>
+					<Menu
+						id='menu-appbar'
+						anchorEl={anchorElNav}
+						anchorOrigin={{
+							vertical: 'bottom',
+							horizontal: 'left',
+						}}
+						keepMounted
+						transformOrigin={{
+							vertical: 'top',
+							horizontal: 'left',
+						}}
+						open={Boolean(anchorElNav)}
+						onClose={handleCloseNavMenu}
+						sx={{ display: { xs: 'block', md: 'none' } }}
+					>
 						{pages.map((page) => (
-                            <Link to={page.path} key={page.id} className='text-center mr-5'>{page.title}</Link>
-							
+							<MenuItem key={page.id} onClick={handleCloseNavMenu}>
+								<Typography sx={{ textAlign: 'center' }}>{page.title}</Typography>
+							</MenuItem>
+						))}
+					</Menu>
+
+					<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+						<IconButton
+							size='large'
+							aria-label='account of current user'
+							aria-controls='menu-appbar'
+							aria-haspopup='true'
+							onClick={handleOpenNavMenu}
+							color='inherit'
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id='menu-appbar'
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: 'bottom',
+								horizontal: 'left',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'left',
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{ display: { xs: 'block', md: 'none' } }}
+						>
+							{pages.map((page) => (
+								<Link to={page.path} key={page.id} className='text-center mr-5'>
+									{page.title}
+								</Link>
+							))}
+						</Menu>
+					</Box>
+
+					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+						{pages.map((page) => (
+							<Link to={page.path} key={page.id} className='text-center mr-5'>
+								{page.title}
+							</Link>
 						))}
 					</Box>
 					<Box sx={{ flexGrow: 0 }}>
@@ -108,7 +172,7 @@ function Header() {
 							<StyledInputBase
 								placeholder='Search…'
 								inputProps={{ 'aria-label': 'search' }}
-								// onChange={(e) => setQuery(e.target.value)}
+								onChange={(e) => setQuery(e.target.value)}
 							/>
 						</Search>
 					</Box>
